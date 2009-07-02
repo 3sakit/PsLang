a608bca984585e8eaf820f11cd425d5f90877058
Charlie <root@jin.localdomain>
2009-07-02 11:41:03 -0500

    added pslang/ast.py this will evolve I am quite sure.

---
+++ pslang/ast.py
@@ -0,0 +1,28 @@
+import pprint
+import excepts
+
+class Namespace:
+	def __init__(self, name=None):
+		if name == None:
+			raise excepts.InvalidArgs, 'While instantiating a Namespace() object.'
+		self.name = name
+		self.children = []
+
+	def new_child(self, name=None):
+		return
+
+class Ast:
+	'''
+	o = Ast()
+	o.dump_namespace()
+	o.new_parent('name')
+	o.dump_namespace()
+	'''
+	def __init__(self):
+		self.namespace = []
+
+	def dump_namespace(self):
+		pprint.pprint(self.namespace)
+
+	def new_parent(self, name)
+		return
